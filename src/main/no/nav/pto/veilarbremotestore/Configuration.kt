af4c10277ee0587db8477f1de5c09410227db987
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-12-03 11:23:08 +0100

    Update nais configuration for prod

--- src/main/no/nav/pto/veilarbremotestore/Configuration.kt
+++ src/main/no/nav/pto/veilarbremotestore/Configuration.kt
@@ -15,7 +15,6 @@ private val defaultProperties = ConfigurationMap(
                 "ISSO_JWKS_URL" to "https://isso-q.adeo.no/isso/oauth2/connect/jwk_uri",
                 "ISSO_ISSUER" to "https://isso-q.adeo.no:443/isso/oauth2",
                 "NAMESPACE" to "q",
-                "AZUREAD_ISSUER" to notUsedLocally,
                 "AZUREAD_JWKS_URL" to notUsedLocally
         )
 )
--- src/main/no/nav/pto/veilarbremotestore/HttpHandler.kt
+++ src/main/no/nav/pto/veilarbremotestore/HttpHandler.kt
@@ -8,7 +8,9 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
 import io.ktor.jackson.JacksonConverter
 import io.ktor.metrics.dropwizard.DropwizardMetrics
-import io.ktor.routing.*
+import io.ktor.request.path
+import io.ktor.routing.route
+import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -16,9 +18,11 @@ import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.dropwizard.DropwizardExports
 import no.nav.pto.veilarbremotestore.JwtUtil.Companion.useJwtFromCookie
 import no.nav.pto.veilarbremotestore.ObjectMapperProvider.Companion.objectMapper
+import no.nav.pto.veilarbremotestore.routes.getNavident
 import no.nav.pto.veilarbremotestore.routes.internalRoutes
 import no.nav.pto.veilarbremotestore.routes.veilarbstoreRoutes
 import no.nav.pto.veilarbremotestore.storage.StorageProvider
+import org.slf4j.event.Level
 
 fun createHttpServer(
     applicationState: ApplicationState,
@@ -76,11 +80,11 @@ fun createHttpServer(
         register(ContentType.Application.Json, JacksonConverter(objectMapper))
     }
 
-    /*install(CallLogging) {
+    install(CallLogging) {
         level = Level.INFO
         filter { call -> !call.request.path().contains("/internal")}
-        //mdc("userId") { applicationCall -> applicationCall.getNavident() }
-    }*/
+        mdc("userId") { applicationCall -> applicationCall.getNavident() }
+    }
 
     install(DropwizardMetrics) {
         CollectorRegistry.defaultRegistry.register(DropwizardExports(registry))
